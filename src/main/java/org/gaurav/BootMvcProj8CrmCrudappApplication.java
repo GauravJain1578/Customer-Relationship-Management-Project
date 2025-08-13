37a7539f2f7aa05c98806a271a1b9da1d3a3ecbb
Gaurav <96946913+GauravJain1578@users.noreply.github.com>
2025-08-13 17:27:03 +0530

    final code

--- src/main/java/org/gaurav/BootMvcProj8CrmCrudappApplication.java
+++ src/main/java/org/gaurav/BootMvcProj8CrmCrudappApplication.java
@@ -7,16 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootMvcProj8CrmCrudappApplication {
 
-//	static {
-//		try {
-//			io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
-//			System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-//			System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-//			System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
-//		} catch (Exception e) {
-//			System.err.println("Could not load .env file: " + e.getMessage());
-//		}
-//	}
+	static {
+		try {
+			io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
+			System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+			System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+			System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		} catch (Exception e) {
+			System.err.println("Could not load .env file: " + e.getMessage());
+		}
+	}
 	public static void main(String[] args) {
 
 		Dotenv dotenv = Dotenv.configure()
